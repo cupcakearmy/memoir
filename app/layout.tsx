c0f8de1f865901c8ed22a0345eab046f9bed2da8
cupcakearmy <hi@nicco.io>
2025-09-06 19:38:18 +0200

    remove banner

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,6 @@ export const metadata = {
   // For more information on metadata API, see: https://nextjs.org/docs/app/building-your-application/optimizing/metadata
 };
 
-const banner = <Banner storageKey="some-key">Nextra 4.0 is released 🎉</Banner>;
 const navbar = (
   <Navbar
     projectLink="https://github.com/cupcakearmy/memoir"
@@ -45,7 +44,6 @@ export default async function RootLayout({ children }) {
           feedback={{
             content: "Question? An error? Give feedback →",
           }}
-          banner={banner}
           navbar={navbar}
           pageMap={await getPageMap()}
           docsRepositoryBase="https://github.com/cupcakearmy/memoir/blob/main"
